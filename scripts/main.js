1ade80d125363b2db659c06f0fa1f4f4990c7fa9
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-02-13 14:22:22 -0500

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,4 @@
-/*
-    The following function makes an api call to get the value for the page counter.
-
-    * I think that I can put a table in the footer of index.html, and split the table into two columns.
-    * Each column will take up 50% of the page.
-    * The first column will be left justified, the second right justified.
-    * I think that the second column will need a zero margin paragraph, so that I can update the page count value that
-    is returned from the api call.  I think the content of a paragraph is called the inner text.
- */
+// The following function makes an api call to get the value for the page counter.
 function updateVisitorCount() {
     // this will call an api to get the visitor count value, but just output random for now.
     let visitorCountVal = Math.random();
@@ -17,4 +9,3 @@ function updateVisitorCount() {
     visitorCountElem.innerText = `Visitor: ${visitorCountVal}`;
     console.log(visitorCountElem.innerText);
 }
-
