2d043a98cd49abf2d2c2f3fa3366e725d2497f1f
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-02-14 13:06:45 -0500

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,9 @@
 // The following function makes an api call to get the value for the page counter.
 //
 function updateVisitorCount() {
-    // this will call an api to get the visitor count value, but just output random for now.
-    let visitorCountVal = Math.random();
-    visitorCountVal = (visitorCountVal * 500) + 1; // between 1 and 500
-    visitorCountVal = Math.floor(visitorCountVal);
-
-    let visitorCountElem = document.getElementById('visitorCount');
-    visitorCountElem.innerText = `Visitor: ${visitorCountVal}`;
-    console.log(visitorCountElem.innerText);
+    fetch('https://38d9ezou2b.execute-api.us-east-2.amazonaws.com/bks-name-visitor-count')
+        .then(response => response.json()).then(data => {
+            console.log(data);
+            document.getElementById('visitorCount').innerText = "Visitor: " + data.toString();
+    });
 }
